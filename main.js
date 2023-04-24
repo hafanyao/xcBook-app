3a7b6939382d88f8c1912722a172d09d82770f1f
hafanyao <shaoqing.yao@featuremaker.com>
2023-04-24 16:06:40 +0800

    add uni-ui & uView

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import App from './App.vue'
+import uviewPlus from '@/uni_modules/uview-plus'
 
 // #ifndef VUE3
 import Vue from 'vue'
@@ -10,10 +11,13 @@ const app = new Vue({
 app.$mount()
 // #endif
 
+
 // #ifdef VUE3
 import {
 	createSSRApp
 } from 'vue'
+app.use(uviewPlus)
+
 export function createApp() {
 	const app = createSSRApp(App)
 	return {
@@ -21,3 +25,23 @@ export function createApp() {
 	}
 }
 // #endif
+
+
+// 需要在app.use(uview-plus)之后执行
+// 调用setConfig方法，方法内部会进行对象属性深度合并，可以放心嵌套配置
+uni.$u.setConfig({
+	// 修改$u.config对象的属性
+	config: {
+		// 修改默认单位为rpx，相当于执行 uni.$u.config.unit = 'rpx'
+		unit: 'rpx'
+	},
+	// 修改$u.props对象的属性
+	props: {
+		// 修改radio组件的size参数的默认值，相当于执行 uni.$u.props.radio.size = 30
+		radio: {
+			size: 15
+		}
+		// 其他组件属性配置
+		// ......
+	}
+})
--- pages/tabbar/tabbar-1/tabbar-1.vue
+++ pages/tabbar/tabbar-1/tabbar-1.vue
@@ -1,6 +1,11 @@
 <template>
 	<view class="content">
 		页面 - 1
+		<uni-badge text="1"></uni-badge>
+		<uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+	<!-- 	<view class="">
+			<u-action-sheet :list="list" v-model="show"></u-action-sheet>
+		</view> -->
 	</view>
 </template>
 
@@ -8,7 +13,17 @@
 export default {
 	data() {
 		return {
-			title: 'Hello'
+			title: 'Hello',
+			list: [{
+				text: '点赞',
+				color: 'blue',
+				fontSize: 28
+			}, {
+				text: '分享'
+			}, {
+				text: '评论'
+			}],
+			show: true
 		};
 	},
 	onLoad() {},
